728996abdc4d0aa64f14806add29bf5aa068e425
Tomek <tomekz@vp.pl>
2021-04-23 14:53:59 +0200

    Add a custom icon, modify the boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>NutritiGuide</h1>
+      <div>
+        Icons made by{' '}
+        <a href='https://www.freepik.com' title='Freepik'>
+          Freepik
+        </a>{' '}
+        from{' '}
+        <a href='https://www.flaticon.com/' title='Flaticon'>
+          www.flaticon.com
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
