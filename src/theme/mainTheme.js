a6d491e7f8209e273468ee624335767d0bae2e03
Tomek <tomekz@vp.pl>
2021-04-27 14:30:36 +0200

    Add a main theme file

---
+++ src/theme/mainTheme.js
@@ -0,0 +1,21 @@
+const theme = {
+  background: 'hsla(82, 74%, 78%, 0.2)',
+  accentDarkGreen: 'hsla(157, 96%, 21%, 0.4)',
+  grey100: 'hsl(0, 0%, 96%)',
+  grey200: 'hsl(0, 0%, 90%)',
+  grey300: 'hsl(0, 0%, 70%)',
+  black: 'hsl(0, 0%, 0%)',
+  light: 300,
+  bold: 600,
+  fontSize: {
+    xxxs: '1rem',
+    xxs: '1.2rem',
+    xs: '1.4rem',
+    s: '1.6rem',
+    m: '2.1rem',
+    l: '2.4rem',
+    xl: '4rem',
+  },
+};
+
+export default theme;
