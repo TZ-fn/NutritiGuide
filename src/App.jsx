b23b7a459fdf800e7a1cb1ebc1ea95a6a1d3bf3f
Tomek <tomekz@vp.pl>
2021-04-28 15:19:37 +0200

    Add a placeholder for the main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import './App.css';
+import Header from 'components/Header/Header';
+import Footer from 'components/Footer/Footer';
+import 'App.css';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <main style={{ height: '700px', backgroundColor: 'hsla(82, 74%, 78%, 0.3)' }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, porro mollitia delectus illo
+        pariatur beatae voluptatum veritatis eos consequuntur tempore, exercitationem, nobis ad
+        architecto placeat? Libero impedit sint tenetur nobis.
+      </main>
       <Footer />
     </div>
   );
