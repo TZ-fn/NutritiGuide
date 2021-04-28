3ed4834783c0271397b8165461f244c3590deed6
Tomek <tomekz@vp.pl>
2021-04-28 14:28:50 +0200

    Refactor Logo text and Logo icon to separate components

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
-import avocadoLogo from '../../../assets/images/avocado.svg';
+import styled from 'styled-components';
+import theme from 'theme/mainTheme';
 
-const Logo = () => <img src={avocadoLogo} alt='NutritiGuide Logo' />;
+const Logo = styled.h1`
+  margin: 0;
+  font-family: Lobster, cursive;
+  font-size: ${theme.fontSize.xl};
+  font-weight: ${theme.fontWeight.regular};
+
+  @media screen and (max-width: 500px) {
+    & {
+      font-size: ${theme.fontSize.s};
+    }
+  }
+`;
 
 export default Logo;
---
+++ src/components/Header/LogoIcon/LogoIcon.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import avocadoLogo from 'assets/images/avocado.svg';
+
+const LogoIcon = () => <img src={avocadoLogo} alt='NutritiGuide Logo' />;
+
+export default LogoIcon;
