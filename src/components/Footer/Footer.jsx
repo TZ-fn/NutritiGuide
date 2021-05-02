4d66bcee294cc973c4a807177f1afbfab1bc1596
Tomek <tomekz@vp.pl>
2021-05-02 21:05:48 +0200

    Add more styling to the Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,54 @@
 import React from 'react';
 import styled from 'styled-components';
-import theme from '../../theme/mainTheme';
+import theme from 'theme/mainTheme';
 
 const Wrapper = styled.footer`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-  background-color: ${theme.background};
+  padding: 1rem 2rem;
+  background: linear-gradient(
+    66deg,
+    ${theme.backgroundLightGreen} 66%,
+    ${theme.backgroundLighterGreen}
+  );
   border-top: 2px solid ${theme.accentDarkGreen};
+  border-radius: 7px 7px 0 0;
+`;
+
+const AttributionsWrapper = styled.div`
+  display: inherit;
+  flex-direction: row;
+  align-items: center;
+  font-size: ${theme.fontSize.x4s};
+
+  & > div {
+    margin: 0 10px;
+  }
+
+  @media (max-width: 480px) {
+    & {
+      transform: scale(0.8);
+    }
+  }
 `;
 
 const Footer = () => (
   <Wrapper>
-    <div>
-      Icons made by{' '}
-      <a href='https://www.freepik.com' title='Freepik'>
-        Freepik
-      </a>{' '}
-      from{' '}
-      <a href='https://www.flaticon.com/' title='Flaticon'>
-        www.flaticon.com
-      </a>
-    </div>
-    <div id='edamam-badge' data-color='light' />
+    <AttributionsWrapper>
+      <div>
+        Icons made by{' '}
+        <a href='https://www.freepik.com' title='Freepik'>
+          Freepik
+        </a>{' '}
+        from{' '}
+        <a href='https://www.flaticon.com/' title='Flaticon'>
+          www.flaticon.com
+        </a>
+      </div>
+      <div id='edamam-badge' data-color='transparent' />
+    </AttributionsWrapper>
   </Wrapper>
 );
 
