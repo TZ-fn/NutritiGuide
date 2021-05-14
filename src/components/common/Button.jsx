14dcdc9d02069bf81c40493e18801ecaa851926e
Tomek <tomekz@vp.pl>
2021-05-14 15:45:56 +0200

    Fix an error with passing onClick props to the Button component

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -33,10 +33,11 @@ const StyledButton = styled.button`
   }
 `;
 
-const Button = ({ children }) => <StyledButton>{children}</StyledButton>;
+const Button = ({ children, onClick }) => <StyledButton onClick={onClick}>{children}</StyledButton>;
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
