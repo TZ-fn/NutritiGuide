3b89ffdcc6ccf9938c840dc0a5e0ecf4ef459fa7
Tomek <tomekz@vp.pl>
2021-05-18 13:00:24 +0200

    Refactor the input-encoding logic

--- src/components/utils/encodeInput.js
+++ src/components/utils/encodeInput.js
@@ -1,10 +1,9 @@
+// encode the user input, so it matches the API requirements
 export default function encodeInput(input) {
   let encodedInput = '';
   encodedInput = input
     .trim()
-    .replace(/,[ ]?/g, '%20and%20')
-    .replace(/\n+/g, '%20and%20')
-    .replace(/ +/g, '%20')
-    .replace(/%20and%20%20and%20/g, '%20and%20');
+    .replace(/[,]?[ ]+?\n+/g, '%20and%20')
+    .replace(/ +/g, '%20');
   return encodedInput;
 }
