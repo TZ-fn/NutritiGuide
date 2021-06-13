a7ce42b1021b4cac1983fc6d9b05cbd6c4645dac
Tomek <tomekz@vp.pl>
2021-06-13 20:23:29 +0200

    Add an ID props for the Checkbox component

--- src/components/Main/AnalysisOptions/AnalysisOptions.jsx
+++ src/components/Main/AnalysisOptions/AnalysisOptions.jsx
@@ -9,30 +9,27 @@ const StyledAnalysisOptionsContainer = styled.div`
   align-items: center;
   justify-content: center;
   min-width: 328px;
-  max-width: 400px;
+  max-width: 380px;
   border: 1px solid black;
+
+  @media (min-width: 1200px) {
+    & {
+      width: 400px;
+    }
+  }
 `;
 
 const AnalysisOptions = () => (
   <StyledAnalysisOptionsContainer>
-    <Checkbox name='proteins' />
-    <Checkbox name='fats' />
-    <Checkbox name='carbs' />
-    <Checkbox name='proteins2' />
-    <Checkbox name='fats3' />
-    <Checkbox name='carbs4' />
-    <Checkbox name='proteins5' />
-    <Checkbox name='fats6' />
-    <Checkbox name='carbs7' />
-    <Checkbox name='proteins' />
-    <Checkbox name='fats' />
-    <Checkbox name='carbs' />
-    <Checkbox name='proteins2' />
-    <Checkbox name='fats3' />
-    <Checkbox name='carbs4' />
-    <Checkbox name='proteins5' />
-    <Checkbox name='fats6' />
-    <Checkbox name='carbs7' />
+    <Checkbox name='proteins' id={1} />
+    <Checkbox name='fats' id={2} />
+    <Checkbox name='carbs' id={3} />
+    <Checkbox name='proteins2' id={4} />
+    <Checkbox name='fats3' id={5} />
+    <Checkbox name='carbs4' id={6} />
+    <Checkbox name='proteins5' id={7} />
+    <Checkbox name='fats6' id={8} />
+    <Checkbox name='carbs7' id={9} />
   </StyledAnalysisOptionsContainer>
 );
 
