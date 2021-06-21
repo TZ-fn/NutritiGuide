02a09108524348a26517b2c0af2f4c119a80a587
Tomek <tomekz@vp.pl>
2021-06-21 15:47:45 +0200

    Add testing
    Add some testing to the project

---
+++ src/App.test.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import optionalTableRows from 'data/optionalTableRows';
+import App from './App';
+
+it('renders the default notification', () => {
+  render(<App />);
+  const defaultNotification = screen.getByText(
+    /Enter your food's ingredients, each ingredient on a new line,/i,
+  );
+  expect(defaultNotification).toBeInTheDocument();
+});
+
+it('renders all the checkboxes options from the optionalTableRows file', () => {
+  render(<App />);
+  const allCheckboxes = optionalTableRows.map((ingredient) => ingredient.name);
+  allCheckboxes.forEach((checkboxName) => {
+    const currentCheckbox = screen.getByText(checkboxName);
+    expect(currentCheckbox).toBeInTheDocument();
+  });
+});
+
+it('fetches and renders the data when the "Analyse" button is pressed', async () => {
+  render(<App />);
+
+  const ingredientsInput = screen.getByPlaceholderText('Enter your ingredient list...');
+  const analyseButton = screen.getByText('Analyse!');
+
+  fireEvent.change(ingredientsInput, {
+    target: { value: '200g butter' },
+  });
+
+  fireEvent.click(analyseButton);
+  await waitFor(() => expect(screen.getByText(/Calories/)).toBeInTheDocument());
+});
