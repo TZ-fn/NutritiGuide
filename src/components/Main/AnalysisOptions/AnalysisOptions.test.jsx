15a3ff036930b0be17faf3a82ab57e65269b6483
Tomek <tomekz@vp.pl>
2021-06-29 15:21:56 +0200

    Add a new test to the AnalysisOptions component
    Add a test that checks the checkboxes functionality

--- src/components/Main/AnalysisOptions/AnalysisOptions.test.jsx
+++ src/components/Main/AnalysisOptions/AnalysisOptions.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, getByText, waitFor } from '@testing-library/react';
 import optionalTableRows from 'data/optionalTableRows';
 import App from 'App';
 
@@ -9,5 +9,28 @@ it('renders all the checkboxes options from the optionalTableRows file', () => {
   allCheckboxes.forEach((checkboxName) => {
     const currentCheckbox = screen.getByText(checkboxName);
     expect(currentCheckbox).toBeInTheDocument();
+  });
+});
+
+it('shows/hides correct table rows when the checkboxes values are changed', async () => {
+  render(<App />);
+  const ingredientsInput = screen.getByPlaceholderText('Enter your ingredients list...');
+  const analyseButton = screen.getByText('Analyse!');
+  const options = screen.getAllByRole('checkbox').map((checkbox) => checkbox.name);
+  let dataTable;
+
+  fireEvent.change(ingredientsInput, {
+    target: { value: '200g butter' },
+  });
+  fireEvent.click(analyseButton);
+
+  await waitFor(() => {
+    dataTable = screen.getByRole('table');
+  });
+
+  options.forEach(async (option) => {
+    const optionCheckbox = screen.getByText(`${option}`);
+    fireEvent.click(optionCheckbox);
+    await waitFor(() => expect(getByText(dataTable, `${option}`)).toBeInTheDocument());
   });
 });
